8e783ef833c1b49c085a3c6b47d17dbae830df1c
Beniamino Tartarini <beniamino.tartarini@gmail.com>
2019-11-29 16:26:56 +0900

    add css plugins and webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const postCSSPlugins = [
+    require("postcss-import"),
     require("postcss-simple-vars"),
     require("postcss-nested"),
     require("autoprefixer")
@@ -12,13 +13,21 @@ module.exports = {
         filename: "bundled.js" ,
         path: path.resolve(__dirname, "app")
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch("./app/**/*.html")
+        },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        host: "0.0.0.0 "
+    },
     mode: "development",
-    watch: true,
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader","css-loader", {loader: "postcss-loader", options: {plugins: postCSSPlugins}}]
+                use: ["style-loader","css-loader?url=false", {loader: "postcss-loader", options: {plugins: postCSSPlugins}}]
             }
         ]
     }
